61bc9f7b9f2c90a20da277e8c0d3f5624a9af629
hexingyi <hexingyi8@gmail.com>
2025-02-13 10:34:55 -0500

    update

--- imcui/ui/app_class.py
+++ imcui/ui/app_class.py
@@ -19,7 +19,7 @@ from .utils import (
     send_to_match,
 )
 import os
-GITHUB_TOKEN = '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'
+GITHUB_TOKEN = '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'
 GOOGLE_TOKEN = '1qnxd5DKomsYUH9_8dQ4Xvwatg_vI-vsh'
 if not (Path(__file__).parent / "../third_party/MatchAnything").exists():
     print("**********************************")
